e820948789e69fc86ea9e694f97e56a1986e36f3
Carlos Pedraza <>
2023-12-29 11:12:14 -0600

    Adding 2024 version of the website

---
+++ seryo.mx/public/js/fechas.js
@@ -0,0 +1,8 @@
+func getJSON(){
+
+	$.getJSON('http://query.yahooapis.com/v1/public/yql?q=select%20%2a%20from%20yahoo.finance.quotes%20WHERE%20symbol%3D%27WRC%27&format=json&diagnostics=true&env=store://datatables.org/alltableswithkeys&callback', function(data) {
+		console.log(data)
+	});
+}
+
+getJSON();
